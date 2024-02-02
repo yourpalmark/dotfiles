80bb15fcd3ce1c930e8f8a6d90e7b87720051e5d
Mark Walters <mark@yourpalmark.com>
2024-02-01 22:39:42 -0500

    Updated cd paths

--- fish/.config/fish/functions/cd_ypm.fish
+++ fish/.config/fish/functions/cd_ypm.fish
@@ -1,3 +1,3 @@
-function cd_yourpalmark
+function cd_ypm
 	cd ~/Library/CloudStorage/GoogleDrive-madwalters@gmail.com/My\ Drive/
 end
---
+++ fish/.config/fish/functions/cd_ypm_repos.fish
@@ -0,0 +1,3 @@
+function cd_ypm_repos
+	cd ~/Library/CloudStorage/GoogleDrive-madwalters@gmail.com/My\ Drive/Repos/personal/yourpalmark/
+end
