d0a501153a263f6180e74664a66b95880539b4fa
Mark Walters <mark@yourpalmark.com>
2025-08-29 01:56:01 -0400

    Updated ypm functions

--- fish/.config/fish/functions/cd_ypm_repos_cloud.fish
+++ fish/.config/fish/functions/cd_ypm_repos_cloud.fish
@@ -1,3 +1,3 @@
-function cd_ypm_repos
+function cd_ypm_repos_cloud
 	cd ~/Library/CloudStorage/GoogleDrive-madwalters@gmail.com/My\ Drive/Repos/personal/yourpalmark/
 end
---
+++ fish/.config/fish/functions/cd_ypm_repos_local.fish
@@ -0,0 +1,3 @@
+function cd_ypm_repos_local
+	cd ~/Local\ Drive/Repos/personal/yourpalmark/
+end
