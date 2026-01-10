4910ace2a23f1c6c66f37eb1166e627775d3d4d8
Mark Walters <mark@yourpalmark.com>
2026-01-10 17:01:36 -0600

    Fish v4.3.x upgrade config changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -g fish_key_bindings fish_default_key_bindings
+
 # Theme
 set -g theme_color_scheme solarized
 set -g theme_nerd_fonts yes
